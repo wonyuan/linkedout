efe0d648eb8316d6c29ffce9b85187d871ce2c9c
shivyam <144865023+shivyam@users.noreply.github.com>
2024-08-03 18:44:30 -0400

    fixed bug with experience scraping

--- backend/scrape.py
+++ backend/scrape.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 import time
@@ -19,11 +22,14 @@ driver.get('https://www.linkedin.com/login')
 driver.find_element("id", "username").send_keys('janoochinki@gmail.com')
 driver.find_element("id", "password").send_keys('passwordhackthe6ix')
 driver.find_element("css selector", '.login__form_action_container button').click()
+time.sleep(20)
 
 # Navigate to the LinkedIn profile page
 profile_url = 'https://www.linkedin.com/in/shivya-mehta'
 driver.get(profile_url)
-time.sleep(10)
+
+time.sleep(5)
+
 # Get the page source
 page_source = driver.page_source
 
@@ -31,17 +37,22 @@ page_source = driver.page_source
 soup = BeautifulSoup(page_source, 'html.parser')
 
 # Extract the name and headline
+time.sleep(1)
 name = soup.find('h1', {'class': 'text-heading-xlarge inline t-24 v-align-middle break-words'}).text.strip()
 time.sleep(1)
 about = soup.find('div', {'class': 'OElnVfzAcygnRqxEDKPEAGXxBkRSBHMbezdNQ inline-show-more-text--is-collapsed inline-show-more-text--is-collapsed-with-line-clamp full-width'}).text.strip()
-time.sleep(10)
-experience_list = soup.find_all('ul', {'class': 'TSXovOouyqHysMDvzEUuMfiyaHtujjHMYdVCA'})
-experience_list_items = experience_list.find_all('li')
-experience_list_items_unique = list(set(experience_list_items))
-for experience in experience_list_items_unique:
-    print(experience.text.strip())         
-
 time.sleep(1)
+experience_list = soup.find('ul', {'class': 'TSXovOouyqHysMDvzEUuMfiyaHtujjHMYdVCA'})
+if experience_list:
+    print("Experience: ")
+    experience_list_items = experience_list.find_all('li')
+    experience_list_items_unique = list(set(experience_list_items))
+    for experience in experience_list_items_unique:
+        if experience.text.strip():
+            print(experience.text.strip())
+else:
+    print("Experience list not found")
+
 
 
 print('Name:', name)
@@ -52,4 +63,4 @@ driver.quit()
 
 
 
-#get education, volunteer experience
+#get education
