43a28838a8445aba2b7effd6175fa6ae3a9c0866
shivyam <144865023+shivyam@users.noreply.github.com>
2024-08-03 16:20:08 -0400

    scrape experiences

--- backend/app.py
+++ backend/app.py
@@ -4,6 +4,7 @@ from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 import time
+
 # Set up Chrome options
 chrome_options = Options()
 
@@ -20,7 +21,7 @@ driver.find_element("id", "password").send_keys('passwordhackthe6ix')
 driver.find_element("css selector", '.login__form_action_container button').click()
 
 # Navigate to the LinkedIn profile page
-profile_url = 'https://www.linkedin.com/in/neha-bhatla'
+profile_url = 'https://www.linkedin.com/in/shivya-mehta'
 driver.get(profile_url)
 time.sleep(10)
 # Get the page source
@@ -33,20 +34,20 @@ soup = BeautifulSoup(page_source, 'html.parser')
 name = soup.find('h1', {'class': 'text-heading-xlarge inline t-24 v-align-middle break-words'}).text.strip()
 time.sleep(1)
 about = soup.find('div', {'class': 'OElnVfzAcygnRqxEDKPEAGXxBkRSBHMbezdNQ inline-show-more-text--is-collapsed inline-show-more-text--is-collapsed-with-line-clamp full-width'}).text.strip()
-time.sleep(1)
-experience_list = soup.find('ul', {'class': 'TSXovOouyqHysMDvzEUuMfiyaHtujjHMYdVCA'})
+time.sleep(10)
+experience_list = soup.find_all('ul', {'class': 'TSXovOouyqHysMDvzEUuMfiyaHtujjHMYdVCA'})
 experience_list_items = experience_list.find_all('li')
 experience_list_items_unique = list(set(experience_list_items))
-
 for experience in experience_list_items_unique:
-    print(experience.text.strip())
+    print(experience.text.strip())         
+
+time.sleep(1)
 
 
-# Print the extracted data
 print('Name:', name)
-print('About:', about)
+print('About Me Section:', about)
 
-# Close the browser
+#closes the browser
 driver.quit()
 
 
